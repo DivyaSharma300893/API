7caccb30bb49137ad692f91d081f32b7f94e2b2d
divya sharma <divya.sharma@weboniselab.com>
2016-09-27 19:04:32 +0530

    Adds show action the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
-Rails.application.routes.draw do
+require 'api_constraints'
+
+MarketPlaceApi::Application.routes.draw do
   devise_for :users
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+              constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+      resources :users, :only => [:show]
+    end
+  end
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
